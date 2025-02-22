b9ee037bd8b9a6ec92b1f8bd926d1180b9b54a83
cesar augusto gadelha <cesar_ags@outlook.com>
2025-02-22 15:23:26 -0300

    Update project version, add icon, and introduce health check
    Updated project version from 1.1.2 to 1.1.3. Added application icon (icone-repo.ico) and included it in the project content. Updated the path for icone-repo.png to a relative path. Added new package references for Microsoft.Extensions.Hosting.Abstractions (8.0.1) and Polly (8.5.2). Removed package reference for MongoDB.Driver.Core (2.30.0).
    
    Introduced MongoDbHealthCheckService class to monitor MongoDB connection health. This class uses dependency injection for MongoDB settings and a logger, and employs Polly for retry policies with exponential backoff. The service periodically checks the MongoDB connection every 30 seconds and logs the results.

---
+++ EasyMongoNet/Services/MongoDbHealthCheckService .cs
@@ -0,0 +1,63 @@
+﻿using EasyMongoNet.Settings;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Polly;
+using Polly.Retry;
+
+namespace EasyMongoNet.Services;
+
+public class MongoDbHealthCheckService : BackgroundService
+{
+    private readonly IMongoClient _mongoClient;
+    private readonly ILogger<MongoDbHealthCheckService> _logger;
+    private readonly AsyncRetryPolicy _retryPolicy;
+    private readonly string _databaseName;
+    private readonly TimeSpan _checkInterval = TimeSpan.FromSeconds(30);
+
+    public MongoDbHealthCheckService(IOptions<MongoDbSettings> settings, ILogger<MongoDbHealthCheckService> logger)
+    {
+        _mongoClient = new MongoClient(settings.Value.ConnectionString);
+        _databaseName = settings.Value.DatabaseName!;
+        _logger = logger;
+
+        _retryPolicy = Policy
+            .Handle<MongoException>()
+            .Or<TimeoutException>()
+            .WaitAndRetryAsync(
+                retryCount: 3,
+                sleepDurationProvider: attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)), // Backoff exponencial
+                onRetry: (exception, timeSpan, retryCount, context) =>
+                {
+                    logger.LogWarning($"[Polly] MongoDB Retry {retryCount} - Waiting {timeSpan.TotalSeconds} sec due to: {exception.Message}");
+                });
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await CheckMongoDbConnectionAsync();
+            await Task.Delay(_checkInterval, stoppingToken);
+        }
+    }
+
+    private async Task CheckMongoDbConnectionAsync()
+    {
+        try
+        {
+            await _retryPolicy.ExecuteAsync(async () =>
+            {
+                var database = _mongoClient.GetDatabase(_databaseName);
+                await database.RunCommandAsync((Command<BsonDocument>)"{ping:1}");
+                _logger.LogInformation("[MongoDbHealthCheck] MongoDB is reachable.");
+            });
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("[MongoDbHealthCheck] MongoDB connection failed: {Message}", ex.Message);
+        }
+    }
+}
